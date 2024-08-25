bd03002ce46a29d66ee26915b661a7d194cec592
Alexanna <alexanna.ec.95@gmail.com>
2024-08-25 10:37:09 +0100

    added in open os serialization, and added nested tables for input

--- lib/configAPI.lua
+++ lib/configAPI.lua
@@ -1,6 +1,7 @@
 local filesystem = require("filesystem")
 local term = require("term")
 local json = require("json")
+local serialization = require("serialization")
 
 local displayAPI = require("displayAPI")
 local printName = "ConfigAPI"
@@ -12,60 +13,69 @@ local configAPI = {}
 
 function configAPI.GetInput(displayTest, default)
     if type(default) == "table" then
+        for i, v in pairs(default) do
+            default[i] = configAPI.GetInput(displayTest .. "[" .. i .. "]", v)
+        end
       return default
     end
 
     term.write(displayTest.." [" .. tostring(default) .. "]: ")
-    input = term.read()
+    local input = term.read()
 
     if input == nil or #input <= 0 then
         return default
     end
     
     input = string.sub(input,1, -2)
-    num = tonumber(input)
+    local num = tonumber(input)
     if num ~= nil then
         return num
     end
     return input
 end
 
-function configAPI.SetupConfig(confName, args, doConfigure)
+function configAPI.SetupConfig(confName, args, doConfigure, useJson)
+    useJson = useJson or false
 
     if(not filesystem.exists(confPath .. confName .. confExtension)) then
-        displayAPI.Print(printName, "Config for '" .. confName .. "' could not be found.\n")
+        displayAPI.Print("Config for '" .. confName .. "' could not be found.\n")
 
         if doConfigure then
-            displayAPI.Print(printName, "Writing defaults\n")
+            displayAPI.Print("Writing defaults\n")
             configAPI.WriteConfFile(confName, args)
             return args
         end
         
-        displayAPI.Print(printName, "Doing setup now:\n")
+        displayAPI.Print("Doing setup now:\n")
         
         for k, v in pairs(args) do
             args[k] = configAPI.GetInput(k, v)
         end
 
-        configAPI.WriteConfFile(confName, args)
+        configAPI.WriteConfFile(confName, args, useJson)
         return args
-    else
-        configAPI.ReadConfFile(confName, args)
-        return args
+    else    
+        return configAPI.ReadConfFile(confName, args, useJson)
     end
 end
 
-function configAPI.ReadConfFile(confName, args)
-    displayAPI.Print(printName, "Reading config: '" .. confName .. "'")
+function configAPI.ReadConfFile(confName, args, useJson)
+    useJson = useJson or false
+    
+    displayAPI.Print("Reading config: '" .. confName .. "'")
 
     local confFile = io.open(confPath .. confName .. confExtension, "r")
 
     if confFile then
-        displayAPI.Print(printName, "Reading File")
+        displayAPI.Print("Reading File")
 
-        data = confFile:read("*a")
-        
-        args = json.decode(data)
+        local data = confFile:read("*a")
+
+        if useJson then
+            args = json.decode(data)
+        else
+            args = serialization.unserialize(data)
+        end
         
         confFile:close()
     end
@@ -73,16 +83,25 @@ function configAPI.ReadConfFile(confName, args)
     return args
 end
 
-function configAPI.WriteConfFile(confName, args)
+function configAPI.WriteConfFile(confName, args, useJson)
+    useJson = useJson or false
+    
     local confFile = io.open(confPath .. confName .. confExtension, "w")
 
     if confFile then
-        data = json.encode(args)
+        local data = ""
+        
+        if useJson then
+            data = json.encode(args)
+        else
+            data = serialization.serialize(args)
+        end
+        
         confFile:write(data)
         confFile:close()
     end
 
-    displayAPI.Print(printName, "Wrote to config: '" .. confName .. "'")
+    displayAPI.Print("Wrote to config: '" .. confName .. "'")
 end
 
 if not filesystem.exists(confPath) then
